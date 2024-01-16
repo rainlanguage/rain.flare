f3448643a4e2a42d9c3040f4f4958cbcd5bd0da5
thedavidmeister <thedavidmeister@gmail.com>
2024-01-16 11:25:04 +0400

    base builds

---
+++ script/Deploy.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script} from "forge-std/Script.sol";
+import {FlareFTSOExtern} from "../src/FlareFTSOExtern.sol";
+
+contract Deploy is Script {
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYMENT_KEY");
+
+        vm.startBroadcast(deployerPrivateKey);
+        new FlareFTSOExtern();
+        vm.stopBroadcast();
+    }
+}
---
+++ test/FlareFTSOExtern.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: CAL
+pragma solidity =0.8.19;
+
+import {Test} from "forge-std/Test.sol";
+import {FlareFTSOExtern} from "../src/FlareFTSOExtern.sol";
+
+contract FlareFTSOExternTest is Test {
+
+}
