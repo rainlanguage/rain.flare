05290e370fd340e90af5cb96ebfd28f124ff0036
thedavidmeister <thedavidmeister@gmail.com>
2024-01-19 11:25:50 +0400

    add sub parser for pair

--- src/abstract/FlareFtsoSubParser.sol
+++ src/abstract/FlareFtsoSubParser.sol
@@ -2,26 +2,32 @@
 pragma solidity =0.8.19;
 
 import {BaseRainterpreterSubParserNPE2, Operand} from "rain.interpreter/abstract/BaseRainterpreterSubParserNPE2.sol";
-import {OPCODE_FTSO_CURRENT_PRICE_USD} from "./FlareFtsoExtern.sol";
+import {OPCODE_FTSO_CURRENT_PRICE_USD, OPCODE_FTSO_CURRENT_PRICE_PAIR} from "./FlareFtsoExtern.sol";
 import {LibSubParse, IInterpreterExternV3} from "rain.interpreter/lib/parse/LibSubParse.sol";
 import {LibParseOperand} from "rain.interpreter/lib/parse/LibParseOperand.sol";
 import {LibConvert} from "rain.lib.typecast/LibConvert.sol";
 import {AuthoringMetaV2} from "rain.interpreter/interface/IParserV1.sol";
 
-bytes constant SUB_PARSER_PARSE_META = hex"01000000000000000000000000000000000000000000080000000000000000000000008057ab";
+bytes constant SUB_PARSER_PARSE_META =
+    hex"01000002000000000000000000000000000000000000080000000000000000000000008057ab015dba81";
 
-bytes constant SUB_PARSER_WORD_PARSERS = hex"06c6";
+bytes constant SUB_PARSER_WORD_PARSERS = hex"07a207c6";
 
-bytes constant SUB_PARSER_OPERAND_HANDLERS = hex"0acd";
+bytes constant SUB_PARSER_OPERAND_HANDLERS = hex"0c170c17";
 
 uint256 constant SUB_PARSER_WORD_FTSO_CURRENT_PRICE_USD = 0;
-uint256 constant SUB_PARSER_WORD_PARSERS_LENGTH = 1;
+uint256 constant SUB_PARSER_WORD_FTSO_CURRENT_PRICE_PAIR = 1;
+uint256 constant SUB_PARSER_WORD_PARSERS_LENGTH = 2;
 
 function authoringMetaV2() pure returns (bytes memory) {
     AuthoringMetaV2[] memory meta = new AuthoringMetaV2[](SUB_PARSER_WORD_PARSERS_LENGTH);
     meta[SUB_PARSER_WORD_FTSO_CURRENT_PRICE_USD] = AuthoringMetaV2(
         "ftso-current-price-usd",
         "Returns the current USD price of the given token according to the FTSO. Accepts 2 inputs, the symbol string used by the FTSO and the timeout in seconds. The price is returned as 18 decimal fixed point number, rounding down if this results in any precision loss. The timeout will be used to determine if the price is stale and revert if it is."
+    );
+    meta[SUB_PARSER_WORD_FTSO_CURRENT_PRICE_PAIR] = AuthoringMetaV2(
+        "ftso-current-price-pair",
+        "Returns the current price of the given token pair according to the FTSO. Accepts 3 inputs, the symbol string used by the FTSO for the base token, the symbol string used by the FTSO for the quote token and the timeout in seconds. The price is returned as 18 decimal fixed point number, rounding down if this results in any precision loss. The timeout will be used to determine if the price is stale and revert if it is. Note that the pair price is derived from two separate FTSO prices mechanically and is not provided directly by the FTSO."
     );
     return abi.encode(meta);
 }
@@ -45,6 +51,7 @@ abstract contract FlareFtsoSubParser is BaseRainterpreterSubParserNPE2 {
         function(uint256[] memory) internal pure returns (Operand)[] memory fs =
             new function(uint256[] memory) internal pure returns (Operand)[](SUB_PARSER_WORD_PARSERS_LENGTH);
         fs[SUB_PARSER_WORD_FTSO_CURRENT_PRICE_USD] = LibParseOperand.handleOperandDisallowed;
+        fs[SUB_PARSER_WORD_FTSO_CURRENT_PRICE_PAIR] = LibParseOperand.handleOperandDisallowed;
 
         uint256[] memory pointers;
         assembly ("memory-safe") {
@@ -59,6 +66,7 @@ abstract contract FlareFtsoSubParser is BaseRainterpreterSubParserNPE2 {
             SUB_PARSER_WORD_PARSERS_LENGTH
         );
         fs[SUB_PARSER_WORD_FTSO_CURRENT_PRICE_USD] = ftsoCurrentPriceUsdSubParser;
+        fs[SUB_PARSER_WORD_FTSO_CURRENT_PRICE_PAIR] = ftsoCurrentPricePairSubParser;
 
         uint256[] memory pointers;
         assembly ("memory-safe") {
@@ -81,6 +89,23 @@ abstract contract FlareFtsoSubParser is BaseRainterpreterSubParserNPE2 {
             1,
             operand,
             OPCODE_FTSO_CURRENT_PRICE_USD
+        );
+    }
+
+    function ftsoCurrentPricePairSubParser(uint256 constantsHeight, uint256 inputsByte, Operand operand)
+        internal
+        view
+        returns (bool, bytes memory, uint256[] memory)
+    {
+        //slither-disable-next-line unused-return
+        return LibSubParse.subParserExtern(
+            IInterpreterExternV3(extern()),
+            constantsHeight,
+            inputsByte,
+            // 1 output for the price.
+            1,
+            operand,
+            OPCODE_FTSO_CURRENT_PRICE_PAIR
         );
     }
 }
