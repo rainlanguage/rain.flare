ee70ec1675133ad723e2ca39b2188f12e58ee7c8
thedavidmeister <thedavidmeister@gmail.com>
2024-09-19 14:19:11 +0400

    lint

--- src/lib/op/LibOpSFlrCurrentExchangeRate.sol
+++ src/lib/op/LibOpSFlrCurrentExchangeRate.sol
@@ -8,7 +8,8 @@ import {
 } from "rain.math.fixedpoint/lib/LibFixedPointDecimalArithmeticOpenZeppelin.sol";
 import {IStakedFlr} from "../../interface/IStakedFlr.sol";
 
-IStakedFlr constant iStakedFlr = IStakedFlr(address(0x12e605bc104e93B45e1aD99F9e555f659051c2BB));
+/// Immutable upgradeable proxy contract to the sFLR contract.
+IStakedFlr constant SFLR_CONTRACT = IStakedFlr(address(0x12e605bc104e93B45e1aD99F9e555f659051c2BB));
 
 /// @title LibOpSLFRCurrentExchangeRate
 /// Implements the `sflrCurrentExchangeRate` externed opcode.
@@ -21,7 +22,7 @@ library LibOpSLFRCurrentExchangeRate {
     /// Extern implementation for reading the current exchange rate of FLR to sFLR
     /// based on directly reading the underlying assets self-reported by the sFLR contract.
     function run(Operand, uint256[] memory) internal view returns (uint256[] memory) {
-        uint256 rate = iStakedFlr.getSharesByPooledFlr(1e18);
+        uint256 rate = SFLR_CONTRACT.getSharesByPooledFlr(1e18);
         uint256[] memory outputs = new uint256[](1);
         outputs[0] = rate;
         return outputs;
