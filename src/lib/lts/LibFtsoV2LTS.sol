86ce0d741ee9166de4ee858511278f2e2f1c4dc9
thedavidmeister <thedavidmeister@gmail.com>
2025-11-15 16:27:38 +0400

    joule feed id

--- src/lib/lts/LibFtsoV2LTS.sol
+++ src/lib/lts/LibFtsoV2LTS.sol
@@ -11,65 +11,68 @@ import {IFeeCalculator} from "flare-smart-contracts-v2/userInterfaces/IFeeCalcul
 /// @dev FTSO feed IDs.
 /// https://dev.flare.network/ftso/feeds
 
-/// @dev FLR/USD feed ID.
+/// @dev 0 FLR/USD feed ID.
 bytes21 constant FLR_USD_FEED_ID = 0x01464c522f55534400000000000000000000000000;
 
-/// @dev SGB/USD feed ID.
+/// @dev 1 SGB/USD feed ID.
 bytes21 constant SGB_USD_FEED_ID = 0x015347422f55534400000000000000000000000000;
 
-/// @dev BTC/USD feed ID.
+/// @dev 2 BTC/USD feed ID.
 bytes21 constant BTC_USD_FEED_ID = 0x014254432f55534400000000000000000000000000;
 
-/// @dev XRP/USD feed ID.
+/// @dev 3 XRP/USD feed ID.
 bytes21 constant XRP_USD_FEED_ID = 0x015852502f55534400000000000000000000000000;
 
-/// @dev LTC/USD feed ID.
+/// @dev 4 LTC/USD feed ID.
 bytes21 constant LTC_USD_FEED_ID = 0x014c54432f55534400000000000000000000000000;
 
-/// @dev XLM/USD feed ID.
+/// @dev 5 XLM/USD feed ID.
 bytes21 constant XLM_USD_FEED_ID = 0x01584c4d2f55534400000000000000000000000000;
 
-/// @dev DOGE/USD feed ID.
+/// @dev 6 DOGE/USD feed ID.
 bytes21 constant DOGE_USD_FEED_ID = 0x01444f47452f555344000000000000000000000000;
 
-/// @dev ADA/USD feed ID.
+/// @dev 7 ADA/USD feed ID.
 bytes21 constant ADA_USD_FEED_ID = 0x014144412f55534400000000000000000000000000;
 
-/// @dev ALGO/USD feed ID.
+/// @dev 8 ALGO/USD feed ID.
 bytes21 constant ALGO_USD_FEED_ID = 0x01414c474f2f555344000000000000000000000000;
 
-/// @dev ETH/USD feed ID.
+/// @dev 9 ETH/USD feed ID.
 bytes21 constant ETH_USD_FEED_ID = 0x014554482f55534400000000000000000000000000;
 
-/// @dev FIL/USD feed ID.
+/// @dev 10 FIL/USD feed ID.
 bytes21 constant FIL_USD_FEED_ID = 0x0146494c2f55534400000000000000000000000000;
 
-/// @dev ARB/USD feed ID.
+/// @dev 11 ARB/USD feed ID.
 bytes21 constant ARB_USD_FEED_ID = 0x014152422f55534400000000000000000000000000;
 
-/// @dev AVAX/USD feed ID.
+/// @dev 12 AVAX/USD feed ID.
 bytes21 constant AVAX_USD_FEED_ID = 0x01415641582f555344000000000000000000000000;
 
-/// @dev BNB/USD feed ID.
+/// @dev 13 BNB/USD feed ID.
 bytes21 constant BNB_USD_FEED_ID = 0x01424e422f55534400000000000000000000000000;
 
-/// @dev POL/USD feed ID.
+/// @dev 14 POL/USD feed ID.
 bytes21 constant POL_USD_FEED_ID = 0x01504f4c2f55534400000000000000000000000000;
 
-/// @dev SOL/USD feed ID.
+/// @dev 15 SOL/USD feed ID.
 bytes21 constant SOL_USD_FEED_ID = 0x01534f4c2f55534400000000000000000000000000;
 
-/// @dev USDC/USD feed ID.
+/// @dev 16 USDC/USD feed ID.
 bytes21 constant USDC_USD_FEED_ID = 0x01555344432f555344000000000000000000000000;
 
-/// @dev USDT/USD feed ID.
+/// @dev 17 USDT/USD feed ID.
 bytes21 constant USDT_USD_FEED_ID = 0x01555344542f555344000000000000000000000000;
 
-/// @dev XDC/USD feed ID.
+/// @dev 18 XDC/USD feed ID.
 bytes21 constant XDC_USD_FEED_ID = 0x015844432f55534400000000000000000000000000;
 
-/// @dev TRX/USD feed ID.
+/// @dev 19 TRX/USD feed ID.
 bytes21 constant TRX_USD_FEED_ID = 0x015452582f55534400000000000000000000000000;
+
+/// @dev 52 JOULE/USD feed ID.
+bytes21 constant JOULE_USD_FEED_ID = 0x014a4f554c452f5553440000000000000000000000;
 
 library LibFtsoV2LTS {
     /// @dev Fetches the value of a feed from the FTSO using V2 LTS.
