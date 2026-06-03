0c185c88ab607ec2e9c71d6276e56799f87a0064
David Meister <thedavidmeister@gmail.com>
2026-06-03 13:17:50 +0000

    legal: add SPDX header to script/build.sh

--- script/build.sh
+++ script/build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# SPDX-License-Identifier: LicenseRef-DCL-1.0
+# SPDX-FileCopyrightText: Copyright (c) 2020 Rain Open Source Software Ltd
 # Regenerate committed meta artifacts that the rainix copy-artifacts reusable
 # diff-checks. Runs in the repo default devshell because `rain` is not in
 # rainix sol-shell.
