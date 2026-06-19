a5d66059437c8ed43f6aef4b257d816a9ec165d0
David Meister <thedavidmeister@gmail.com>
2026-06-19 21:14:37 +0000

    fix(ci): rainix-sol-test [3b-attempt]: correct SLFR→SFlr symbol names in test imports

--- test/src/concrete/FlareFtsoWords.wordOpcodeAlignment.t.sol
+++ test/src/concrete/FlareFtsoWords.wordOpcodeAlignment.t.sol
@@ -13,7 +13,7 @@ import {
 import {
     OPCODE_FTSO_CURRENT_PRICE_USD,
     OPCODE_FTSO_CURRENT_PRICE_PAIR,
-    OPCODE_SLFR_CURRENT_EXCHANGE_RATE,
+    OPCODE_SFLR_CURRENT_EXCHANGE_RATE,
     OPCODE_FUNCTION_POINTERS_LENGTH
 } from "src/abstract/FlareFtsoExtern.sol";
 import {AuthoringMetaV2} from "rain-interpreter-interface-0.1.0/src/interface/deprecated/v1/IParserV1.sol";
@@ -22,7 +22,7 @@ contract FlareFtsoWordsWordOpcodeAlignmentTest is Test {
     function testWordOpcodeIndicesAligned() external pure {
         assertEq(SUB_PARSER_WORD_FTSO_CURRENT_PRICE_USD, OPCODE_FTSO_CURRENT_PRICE_USD);
         assertEq(SUB_PARSER_WORD_FTSO_CURRENT_PRICE_PAIR, OPCODE_FTSO_CURRENT_PRICE_PAIR);
-        assertEq(SUB_PARSER_WORD_SFLR_EXCHANGE_RATE, OPCODE_SLFR_CURRENT_EXCHANGE_RATE);
+        assertEq(SUB_PARSER_WORD_SFLR_EXCHANGE_RATE, OPCODE_SFLR_CURRENT_EXCHANGE_RATE);
         assertEq(SUB_PARSER_WORD_PARSERS_LENGTH, OPCODE_FUNCTION_POINTERS_LENGTH);
     }
 
@@ -30,6 +30,6 @@ contract FlareFtsoWordsWordOpcodeAlignmentTest is Test {
         AuthoringMetaV2[] memory m = abi.decode(LibFlareFtsoSubParser.authoringMetaV2(), (AuthoringMetaV2[]));
         assertEq(m[OPCODE_FTSO_CURRENT_PRICE_USD].word, bytes32("ftso-current-price-usd"));
         assertEq(m[OPCODE_FTSO_CURRENT_PRICE_PAIR].word, bytes32("ftso-current-price-pair"));
-        assertEq(m[OPCODE_SLFR_CURRENT_EXCHANGE_RATE].word, bytes32("sflr-exchange-rate"));
+        assertEq(m[OPCODE_SFLR_CURRENT_EXCHANGE_RATE].word, bytes32("sflr-exchange-rate"));
     }
 }
